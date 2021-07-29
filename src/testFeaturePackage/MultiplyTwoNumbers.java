f9c4de9d175ff91bb75a24739da9e7b04ea99337
uv1406 <test@tester.com>
2021-07-29 20:13:55 +0530

    Commit multiply to BranchX for multiply code.

---
+++ src/testFeaturePackage/MultiplyTwoNumbers.java
@@ -0,0 +1,12 @@
+package testFeaturePackage;
+
+public class MultiplyTwoNumbers {
+	 public static void main(String[] args) {
+	        
+	      int num1 = 5, num2 = 15, sum;
+	      sum = num1 * num2;
+
+	      System.out.println(" Branch x Sum of these numbers: "+sum);
+	   }
+
+}
