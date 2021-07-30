e1a6afaef4032f0e7462390a1a18fac264c0d3c7
uv1407 <ujjawalverma@ujjawal3628-01.Nagarro.local>
2021-07-30 06:33:45 +0530

    Added additional code for division.

--- src/testFeaturePackage/MultiplyTwoNumbers.java
+++ src/testFeaturePackage/MultiplyTwoNumbers.java
@@ -3,10 +3,10 @@ package testFeaturePackage;
 public class MultiplyTwoNumbers {
 	 public static void main(String[] args) {
 	        
-	      int num1 = 5, num2 = 15, sum;
-	      sum = num1 * num2;
-
-	      System.out.println(" Branch x Sum of these numbers: "+sum);
+	      int num1 = 5, num2 = 15, multiply,div;
+	      multiply = num1 * num2;
+	      div = num1 / num2;
+	      System.out.println(" Branch x Sum of these numbers: "+multiply + div);
 	   }
 
 }
