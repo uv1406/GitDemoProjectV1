5c14e4bf082d1ec621adef95ad35f558ebc10032
Ujjawal Verma <ujjawal.verma@nagarro.com>
2021-08-03 06:50:01 +0530

    Fixed automation bug in this branch

--- src/testFeaturePackage/MultiplyTwoNumbers.java
+++ src/testFeaturePackage/MultiplyTwoNumbers.java
@@ -3,11 +3,15 @@ package testFeaturePackage;
 public class MultiplyTwoNumbers {
 	 public static void main(String[] args) {
 	        
-	      int num1 = 5, num2 = 15, multiply,div;
-	      multiply = num1 * num2;
-	      div = num1 / num2;
-	      System.out.println(" Branch x Sum of these numbers: "+multiply );
-	      System.out.println(" Branch x Sum of these numbers: "+div);
+	      int num1 = 5, num2 = 15,num3 =999, multiply = 0,div;
+	      
+	      div = num1 / num2/num3;
+	      
+	      for (int i=1; i<20;i++) {
+	    	  multiply = i*num1*num2*num3;
+	      }
+	      System.out.println(" Branch Bug Fix_automation of these numbers: "+multiply );
+	      System.out.println(" Branch Bug Fix_automation Sum of these numbers: "+div);
 	   }
 
 }
